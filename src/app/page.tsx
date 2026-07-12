e2a6614dd4f999feb1e8cd7d9f49a54b6d58a61b
jatnielhatma-a11y <jatnielhatma@gmail.com>
2026-07-13 01:34:21 +0200

    Link Emma OCC to Yu'i Korsou Translate Lab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Link from 'next/link';
 import OccDashboard from '@/components/OccDashboard';
 
 export default function Home() {
-  return <OccDashboard />;
+  return (
+    <>
+      <OccDashboard />
+      <Link
+        href="/translate"
+        className="connect-google"
+        style={{ position: 'fixed', right: 20, bottom: 20, zIndex: 20, boxShadow: '0 12px 30px rgba(0,0,0,.25)' }}
+      >
+        🇨🇼 Open NOVA Translate Lab
+      </Link>
+    </>
+  );
 }
